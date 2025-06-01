e9fc06b979380ca5b82dd40cbb6bc164d53b047f
Insaf-Finser <insaffinser@gmail.com>
2025-06-01 23:02:11 +0530

    navbar half

--- lib/models/rive_item_model.dart
+++ lib/models/rive_item_model.dart
@@ -1,84 +1,52 @@
 import 'rive_model.dart';
 
-class NavToolModel{
+class NavItemModel {
   final String title;
   final RiveModel rive;
 
-  NavToolModel({
+  NavItemModel({
     required this.title,
     required this.rive,
   });
 }
 
-List<NavToolModel> navTools = [
-  NavToolModel(
+List<NavItemModel> bottomNavItems = [
+  NavItemModel(
     title: 'Chat',
     rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
+      src: 'assets/animations/animated_icons.riv',
       artboard: 'CHAT',
       stateMachineName: 'CHAT_Interactivity',
     ),
   ),
-  NavToolModel(
+  NavItemModel(
     title: 'Search',
     rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
+      src: 'assets/animations/animated_icons.riv',
       artboard: 'SEARCH',
       stateMachineName: 'SEARCH_Interactivity',
     ),
   ),
-  NavToolModel(
-    title: 'Timer',
-    rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
-      artboard: 'TIMER',
-      stateMachineName: 'TIMER_Interactivity',
-    ),
-  ),
-  NavToolModel(
-    title: 'Reload',
-    rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
-      artboard: 'RELOAD',
-      stateMachineName: 'RELOAD_Interactivity',
-    ),
-  ),
-  NavToolModel(
+  NavItemModel(
     title: 'Home',
     rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
+      src: 'assets/animations/animated_icons.riv',
       artboard: 'HOME',
-      stateMachineName: 'HOME_Interactivity',
+      stateMachineName: 'HOME_interactivity',
     ),
   ),
-  NavToolModel(
-    title: 'Star',
-    rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
-      artboard: 'STAR',
-      stateMachineName: 'STAR_Interactivity',
-    ),
-  ),
-  NavToolModel(
-    title: 'Audio',
-    rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
-      artboard: 'AUDIO',
-      stateMachineName: 'AUDIO_Interactivity',
-    ),
-  ),
-  NavToolModel(
+  NavItemModel(
     title: 'Settings',
     rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
+      src: 'assets/animations/animated_icons.riv',
       artboard: 'SETTINGS',
       stateMachineName: 'SETTINGS_Interactivity',
     ),
   ),
-  NavToolModel(
+  NavItemModel(
     title: 'User',
     rive: RiveModel(
-      src: 'assets/RiveAssets/icons.riv',
+      src: 'assets/animations/animated_icons.riv',
       artboard: 'USER',
       stateMachineName: 'USER_Interactivity',
     ),
--- lib/presentaion/menu/menu.dart
+++ lib/presentaion/menu/menu.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:transfer/models/rive_item_model.dart';
 
-class MenuPage extends StatelessWidget {
-  const MenuPage({Key? key}) : super(key: key);
 
-  static const List<_MenuItem> _menuItems = [
-    _MenuItem(icon: Icons.chat, label: '', route: '/chats'),
-    _MenuItem(icon: Icons.group, label: '', route: '/groups'),
-    _MenuItem(icon:Icons.add , label: '', route: '/new_chat'),
-    _MenuItem(icon: Icons.settings, label: '', route: '/settings'),
-    _MenuItem(icon: Icons.person, label: '', route: '/profile'),
-  ];
+class MenuPage extends StatefulWidget {
+  const MenuPage({super.key});
+
+  @override
+  State<MenuPage> createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  List<SMIBool?> riveIconsInputs = [];
+
+  int selectedNavIndex = 0;
+
+  void animateIcons(index){
+    final input = riveIconsInputs[index];
+    if (input != null) {
+      input.change(true);
+      Future.delayed(const Duration(seconds: 1), () {
+        input.change(false);
+      });
+    }
+  }
+
+  void rivInit(Artboard artboard, int index, dynamic riveItem) {
+    StateMachineController? controller = StateMachineController.fromArtboard(
+                          artboard,
+                          riveItem,
+                        );
+                        if (controller != null) {
+                          artboard.addController(controller);
+                          final input = controller.findInput<bool>('active') as SMIBool?;
+                          setState(() {
+                            riveIconsInputs[index] = input;
+                          });
+                        }
+  }
+  
+  @override
+  void initState() {
+    super.initState();
+    riveIconsInputs = List<SMIBool?>.filled(bottomNavItems.length, null);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,30 +57,88 @@ class MenuPage extends StatelessWidget {
           style: Theme.of(context).textTheme.titleMedium,
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: _menuItems
-            .map((item) => BottomNavigationBarItem(
-                  icon: Icon(item.icon),
-                  label: item.label,
-                ))
-            .toList(),
-        onTap: (index) {
-          Navigator.pushNamed(context, _menuItems[index].route);
-        },
-        type: BottomNavigationBarType.fixed,
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 8.0), // Add padding to prevent overflow
+          child: Container(
+            height: 66,
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8), // Reduce vertical margin
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(227, 70, 70, 70),
+              borderRadius: const BorderRadius.all(Radius.circular(24)),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(51, 0, 0, 0), // 0.2 * 255 = 51
+                  blurRadius: 20,
+                  offset: const Offset(0, 20),
+                  // changes position of shadow
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(
+                bottomNavItems.length,
+                (index) {
+                  final riveItem = bottomNavItems[index].rive;
+            
+                  return GestureDetector(
+                    onTap: (){
+                      animateIcons(index);
+                      setState(() {
+                        selectedNavIndex = index;
+                      });
+                    },
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        AnimatedBar(isActive: selectedNavIndex == index),
+                        Opacity(
+                          opacity: selectedNavIndex == index ? 1 : 0.5,
+                          child: SizedBox(
+                            width: 36,
+                            height: 36,
+                            child: RiveAnimation.asset(
+                              riveItem.src,
+                              artboard: riveItem.artboard,
+                              onInit: (artboard) {
+                                rivInit(artboard, index, riveItem.stateMachineName);
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
-class _MenuItem {
-  final IconData icon;
-  final String label;
-  final String route;
-
-  const _MenuItem({
-    required this.icon,
-    required this.label,
-    required this.route,
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    super.key,
+    required this.isActive ,
   });
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: isActive ? 20 : 0, // Adjust width based on isActive
+      height: 4,
+      margin: const EdgeInsets.only(bottom: 2),
+      decoration: BoxDecoration(
+        color: const Color.from(alpha: 1, red: 0.537, green: 0.898, blue: 0.961),
+        borderRadius: BorderRadius.circular(2),
+      ),
+    );
+  }
 }
